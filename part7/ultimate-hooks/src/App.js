0fb2b822d49707a58716ae33d418280bc2b179eb
devfromfinland <contact@vietphan.info>
2020-06-27 01:24:35 +0300

    complete exercise 7.8

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -10,20 +10,44 @@ const useField = (type) => {
     setValue(event.target.value)
   }
 
+  const reset = () => {
+    setValue('')
+  }
+
   return {
     type,
     value,
-    onChange
+    onChange,
+    reset
   }
 }
 
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const fetchData = async () => {
+      // console.log('baseUrl', baseUrl)
+      try {
+        const response = await axios.get(baseUrl)
+      // console.log('fetched data', response.data)
+      setResources(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-  const create = (resource) => {
-    // ...
+    fetchData()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    try {
+      const result = await axios.post(baseUrl, resource)
+      // console.log('result', result)
+      setResources([ ...resources, result.data ])
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   const service = {
@@ -37,8 +61,16 @@ const useResource = (baseUrl) => {
 
 const App = () => {
   const content = useField('text')
+  const mainContent = Object.assign({}, content)
+  delete mainContent.reset
+
   const name = useField('text')
+  const mainName = Object.assign({}, name)
+  delete mainName.reset
+
   const number = useField('text')
+  const mainNumber = Object.assign({}, number)
+  delete mainNumber.reset
 
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
@@ -46,26 +78,29 @@ const App = () => {
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
+    content.reset()
   }
  
   const handlePersonSubmit = (event) => {
     event.preventDefault()
     personService.create({ name: name.value, number: number.value})
+    name.reset()
+    number.reset()
   }
 
   return (
     <div>
       <h2>notes</h2>
       <form onSubmit={handleNoteSubmit}>
-        <input {...content} />
+        <input {...mainContent} />
         <button>create</button>
       </form>
       {notes.map(n => <p key={n.id}>{n.content}</p>)}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
-        number <input {...number} />
+        name <input {...mainName} /> <br/>
+        number <input {...mainNumber} />
         <button>create</button>
       </form>
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
